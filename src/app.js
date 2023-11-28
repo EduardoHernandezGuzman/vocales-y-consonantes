e7dd55889733dfb08ebec0dfac4cccd6c96b671d
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-28 16:38:27 +0000

    Logica y aspecto visual ok

--- src/app.js
+++ src/app.js
@@ -1,11 +1,45 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const result = document.querySelector(".result");
+const btn = document.querySelector("#buttonClick");
+const texto = document.querySelector(".form-control");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+btn.addEventListener("click", contador);
+
+function contador() {
+  if (!texto.value) {
+    return;
+  }
+
+  let vowelCount = 0;
+  let wordVal = texto.value.toLowerCase();
+
+  let consonantCount = 0;
+  let consonantVal = texto.value.toLowerCase();
+
+  for (let i = 0; i < wordVal.length; i++) {
+    let letterVowel = wordVal[i];
+
+    if (letterVowel.match(/([a,e,i,o,u,á,é,í,ó,ú,ä,ë,ï,ö,ü])/)) {
+      vowelCount++;
+    }
+  }
+
+  for (let f = 0; f < consonantVal.length; f++) {
+    let letterConsonant = consonantVal[f];
+    if (
+      letterConsonant.match(
+        /(["b","c","d","f","g","h","j","k","l","m","n","ñ","p","q","r","s","t","v","w","x","y","z"])/
+      )
+    ) {
+      consonantCount++;
+    }
+  }
+
+  result.innerHTML = `
+    <strong >${texto.value}</strong> tiene
+    <strong class="destacado1">${vowelCount}</strong> vocales y
+    <strong class="destacado1">${consonantCount}</strong> consonantes
+  `;
+}
